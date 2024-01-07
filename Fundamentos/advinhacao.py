22af7ae80cb818afe98d4aaf0602d4f87848dbd2
DevLeoFulco <devleofulco@gmail.com>
2024-01-07 00:36:25 -0300

    Foram adicionados os pontos iniciais do usuario e feito os cauculos de pontuação geral nas chamada da condição de acerto e no bloco de condicionais elife referente aos erros do numero de advinhação

--- Fundamentos/advinhacao.py
+++ Fundamentos/advinhacao.py
@@ -5,9 +5,21 @@ print("---- Bem vindo ao Jogo ---")
 print("--------------------------")
 
 numero_secreto = round(random.randrange(1,101))
-total_de_tentativas = 5
+total_de_tentativas = 0
+pontos = 1000
 
+print ("Qual o nível de dificuldade deseja jogar?")
+print("Digite: 1 - Para nível FÁCIL; 2- Para nível MÉDIO; 3- Para nível DIFÍCIL")
+nivel = int(input("Qual o nível: "))
 
+if (nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 12
+elif(nivel == 3):
+    total_de_tentativas = 5
+else:
+    print("Você precisa escolher o nível 1, 2 ou 3")
 
 for rodadas in range(total_de_tentativas+1):
     print("Tentativa {} de {}".format(rodadas,total_de_tentativas))
@@ -24,12 +36,15 @@ for rodadas in range(total_de_tentativas+1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou! Sua pontuação total foi {} pontos".format(pontos))
         break
     else:
         if (maior):
             print("Você errou: o seu chute foi maior que o numero secreto")
         elif(menor):
             print("Você errou: o seu chute foi menor que o numero secreto")
+
+        pontos_perdido = abs(numero_secreto-chute)
+        pontos -= pontos_perdido
 
 print("Fim do jogo!")
